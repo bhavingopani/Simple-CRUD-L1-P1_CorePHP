983c9c0367b814f337b976973b1a447ad3fbdd35
bhavingopani <gopani7874@gmail.com>
2022-05-02 19:39:50 +0530

    Simple CrudApp Project1Level1 CORE PHP
    Did all the validation except the unique email thing.. The main thing took time was the pagination next button as the loop variable's last value was affecting the variable of the next button of the pagination. Hence, did the next and previous thing before the loop runs. Did the validation in front and backend.

--- testing/create_user.php
+++ testing/create_user.php
@@ -1,5 +1,3 @@
-
-
 <?php
 #save all the things and save data in the database
 
@@ -18,39 +16,77 @@ if ($connection->connect_error) {
     die("Connection failed: " .  $connection->connect_error); #will throgh an exception and print the same
 }
 
-echo "Database Connected Successfully";  #if no errors then.... will show print this message.
+// echo "Database Connected Successfully";  #if no errors then.... will show print this message.
 
 
 
 
 //checking if its a POST request -- otherwise it will start creating empty records.
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-//saving all the data coming from the from to the respective variables.
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$confirm_password = $_POST['confirm_password'];
+    //for validation err.. added this after writing logic as we will have to need this.. later
+    $anyErr = $firstNameErr = $emailErr = $passwordErr = $confirmPasswordErr = "";
+    $first_name = $email = $password = $confirm_password = ""; #assuming empty so that we don't have warning of undefined variable. 
 
+    //saving all the data coming from the from to the respective variables.
+    if (empty($_POST['first_name'])) {
+        $firstNameErr = "First name is required";
+        // echo "First name needed";
+    } else {
+        $first_name = $_POST['first_name'];
+        // echo "First name needed";
+    }
 
+    $last_name = $_POST['last_name'];
 
-//Writing query and saving it to sql variable.
-$sql = "INSERT INTO create_user (first_name, last_name, email, password)
-            VALUES ('$first_name', '$last_name', '$email', '$password')";
+    if (empty($_POST['email'])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = $_POST['email'];
+    }
 
-//Executing the above query and it will create row in the table.
-if($connection->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $connection->error;
+    if (empty($_POST['password'])) {
+        $passwordErr = "Password is required";
+    } else {
+        $password = $_POST['password'];
+    }
+
+    if (empty($_POST['confirm_password'])) {
+        $confirmPasswordErr = "Confirm Password is required";
+    } else {
+        $confirm_password = $_POST['confirm_password'];
+    }
+
+    $confirm_password = $_POST['confirm_password'];
+
+    // if (empty($anyErr)){
+    //     echo "the value is there";
+    // }
+    // echo $firstnameErr;
+    if ($firstNameErr == "" and $emailErr =="" and $passwordErr=="" and $confirmPasswordErr=="") {
+        //Writing query and saving it to sql variable.
+        // echo "What?";
+        
+        $sql = "INSERT INTO create_user (first_name, last_name, email, password)
+        VALUES ('$first_name', '$last_name', '$email', '$password')";
+        
+        //Executing the above query and it will create row in the table.
+        if ($connection->query($sql) === TRUE) {
+            $success_message = "New record created successfully";
+        } else {
+            $db_error_message = "Error: " . $sql . "<br>" . $connection->error;
+        }
+        $connection->close();
+        //Closing the connection once we dont need it
+        // $connection->close();
+    } else{
+        $connection->close();
+        // echo "connection closed";
+    }  
 }
 
-//Closing the connection once we dont need it
-$connection->close();
 
 
-}
 
 
 ?>
@@ -61,6 +97,12 @@ $connection->close();
 
 <html>
 
+<style>
+    .error {
+        color: red;
+    }
+</style>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -69,10 +111,19 @@ $connection->close();
 </head>
 
 <body>
-    <form  method="POST">
+    <form method="POST">
+        
         <div>
             <label for="first_name"> First Name </label></br>
             <input type="text" name="first_name">
+            <span class="error">
+                <?php
+                if (isset($firstNameErr)) {
+                    echo $firstNameErr;
+                }
+                ?>
+
+            </span>
         </div>
 
         </br>
@@ -82,11 +133,18 @@ $connection->close();
             <input type="text" name="last_name">
         </div>
         </br>
-        
-        
+
+
         <div>
             <label> Email </label></br>
             <input type="text" name="email">
+            <span class="error">
+                <?php
+                if (isset($emailErr)) {
+                    echo $emailErr;
+                }
+                ?>
+            </span>
         </div>
         </br>
 
@@ -95,29 +153,56 @@ $connection->close();
         <div>
             <label> Password </label></br>
             <input type="password" name="password">
+            <span class="error">
+                <?php
+                if (isset($passwordErr)) {
+                    echo $passwordErr;
+                }
+                ?>
+            </span>
         </div>
         </br>
         <div>
             <label> Confirm Password </label></br>
             <input type="password" name="confirm_password">
+            <span class="error">
+                <?php
+                if (isset($confirmPasswordErr)) {
+                    echo $confirmPasswordErr;
+                }
+                ?>
+            </span>
         </div>
         </br>
         <div>
-            <input type="submit" >
+            <input type="submit">
         </div>
     </form>
-</br>
+    </br>
+    <div class= "success">
+        <?php 
+        if (isset($success_message)){
+            echo $success_message; 
+        }
+        ?>
+    </div>
+    <div class= "error">
+        <?php 
+        if (isset($db_error_message)){
+            echo $db_error_message; 
+        }
+        ?>
+    </div>
 
 
-
-<pre>
+    <pre>
     DATA from GET request
 <?php
 print_r($_GET)
 ?>
 </pre>
 
-<pre>
+    <pre>
     DATA from POST request
 <?php
 print_r($_POST)
--- testing/user_list.php
+++ testing/user_list.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 //have to user variable assign all database details
 $servername = "localhost";
@@ -17,7 +17,7 @@ if ($connection->connect_error) {
 // echo "Database Connected Successfully";  #if no errors then.... will show print this message.
 
 
-$sql ="SELECT * FROM create_user";
+$sql = "SELECT * FROM create_user";
 
 // $result = mysqli_query($connection, $sql);
 $result = $connection->query($sql);
@@ -32,11 +32,11 @@ $results_per_page = 10;
 
 $number_of_results = mysqli_num_rows($result); #this will give number of rows 
 
-$number_of_page = ceil( $number_of_results/$results_per_page ); #number of page #ceil gives rounds the number to the nearest integer
+$number_of_page = ceil($number_of_results / $results_per_page); #number of page #ceil gives rounds the number to the nearest integer
 
 #get the current page user is on -- if no records then by default its 1
 
-if(!isset($_GET['page'])) {
+if (!isset($_GET['page'])) {
     $page = 1;
 } else {
     $page = $_GET['page'];
@@ -44,10 +44,10 @@ if(!isset($_GET['page'])) {
 
 #Set limit for 1 page.. here its 10
 
-$page_first_result = ($page - 1)* $results_per_page;
+$page_first_result = ($page - 1) * $results_per_page;
 
 
-$sql_new ="SELECT * FROM create_user LIMIT $page_first_result , $results_per_page";
+$sql_new = "SELECT * FROM create_user LIMIT $page_first_result , $results_per_page";
 
 
 
@@ -74,81 +74,122 @@ $result_new = $connection->query($sql_new);
 ?>
 
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User List</title>
-    <style type="text/css"> 
-        table, th, td,tr {
-        border: 1px solid black;
-        border-collapse: collapse;
-        margin: auto;
-        padding:20px;
-        /* padding-left: 10px;
+    <style type="text/css">
+        table,
+        th,
+        td,
+        tr {
+            border: 1px solid black;
+            border-collapse: collapse;
+            margin: auto;
+            padding: 20px;
+            /* padding-left: 10px;
         padding-right: 10px;
         padding-top: 10px;
         padding-bottom: 10px;         */
         }
+
+        div {
+            text-align: center;
+        }
+
+        /* .new {
+            text-align: left;
+            margin: auto;
+        } */
     </style>
 
 </head>
+
 <body>
-    
-<div >
-<table>
-    <tr>
-    <th> id </th>
-    <th> First Name </th>
-    <th> Last Name </th>
-    <th> Email </th>
-    <th>Edit</th>
-    <th>Delete</th>
-    </tr>
-    
-    <?php
-    while ($row_new= mysqli_fetch_array($result_new)){
 
-        echo "<tr>";
-        echo "<td>". $row_new['id'] . "</td>";
-        echo "<td>" . $row_new['first_name'] . "</td>";
-        echo "<td>". $row_new['last_name'] . "</td>";
-        echo "<td>" . $row_new['email']. "</td>";
-        echo "<td><a href=" . $row_new['id'] . ">" .  "Edit </a></td>";
-        echo "<td><a href=" . $row_new['id'] . ">" .  "Delete </a></td>";
-        echo "</tr>";
+    <div>
+        <table>
+            <tr>
+                <th> id </th>
+                <th> First Name </th>
+                <th> Last Name </th>
+                <th> Email </th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
 
-    }
+            <?php
+            while ($row_new = mysqli_fetch_array($result_new)) {
 
-
-    ?>
-</table>
-</div>
+                echo "<tr>";
+                echo "<td>" . $row_new['id'] . "</td>";
+                echo "<td>" . $row_new['first_name'] . "</td>";
+                echo "<td>" . $row_new['last_name'] . "</td>";
+                echo "<td>" . $row_new['email'] . "</td>";
+                echo "<td><a href=" . $row_new['id'] . ">" .  "Edit </a></td>";
+                echo "<td><a href=" . $row_new['id'] . ">" .  "Delete </a></td>";
+                echo "</tr>";
+            }
 
 
-
-<?php
+            ?>
+        </table>
+    </div>
 
 
 
-    #Display link of the urls of the pages.    
-    
 
-// if (    );        
+    <div>
 
-    
-    echo "<a href=''>" . "Next" ." </a>";
-    
-    "<div>";
-    for($page=1; $page<=$number_of_page; $page++) {
-        echo "<a href='user_list.php?page=$page'>" . $page . "</a>" . "&nbsp";
-    }
-    "</div>";
 
-    echo "<a href=''>" . "Previous" ." </a>";
-?>
+        <!-- #Display link of the urls of the pages. -->
 
-START WITH PAGINAATION ---  LINKING AND --ALL
+        <?php
+        // echo $page;
+        if ($page <= 1) {
+            echo "<a style='visibility:hidden;'>" . "Previous" . " </a>";
+        } else {
+            // echo $page;
+            $previous_page = $page - 1;
+            echo "<a href='?page=$previous_page'>" . "Previous" . "</a>";
+        }
+
+        ?>
+
+        <?php
+        // echo $page;
+        // echo $number_of_page;
+        if ($page >= $number_of_page) {
+            echo "<a style='visibility:hidden;'>" . "Next" . " </a>";
+        } else {
+            // echo $page;
+            $next_page = $page + 1;
+            echo "<a href='?page=$next_page'>" . "Next" . " </a>";
+        }
+        ?>
+    </div>
+    <div>
+        <?php
+        for ($page = 1; $page <= $number_of_page; $page++) {
+            echo "<a href=?page=$page>" . $page . "</a>" . "&nbsp";
+            // return 1;
+        
+        }
+        // echo $page;  #WHY IT STILL KEEPS THE VALUE OUTSIDE OF THE LOOP - CHECK AND ASK LATER. #hence cannot use the logic of next button after this as this has wrong value.
+        ?>
+    </div>
+    </br>
+    <div class='new'>
+        <a href="/create_user.php"> Creat User </a>
+    </div>
+
+
+    <!-- <ul>
+        <a href="?page= "
+        </ul> -->
 
 </body>
+
 </html>
